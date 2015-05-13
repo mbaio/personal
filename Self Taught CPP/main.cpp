0330135a62addd7f912c50b1b4caccc8bb0606a7
Michael Baio <MichaelBaio@Michaels-MacBook-Pro-5.local>
2015-05-13 17:44:03 -0500

    Initial Commit

---
+++ Self Taught CPP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Self Taught CPP
+//
+//  Created by Michael Baio on 5/13/15.
+//  Copyright (c) 2015 Michael Baio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
